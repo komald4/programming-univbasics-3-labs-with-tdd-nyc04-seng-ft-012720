3e598b1777ec212b39ed7e1a869efcdae1021a54
calm-fork-7535 <komald.4@gmail.com>
2020-01-15 19:52:10 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,6 +2,6 @@ first_number = 4
 second_number = 4.5
 
 sum = first_number + second_number
-subtracting = first_number - second_number
+difference = first_number - second_number
 multiplication = first_number * second_number
 division = first_number * second_number
